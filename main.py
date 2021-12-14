47616997d9bdbc3e24c19a94a96014e8e88ffcd2
Antr <65603607+AntyElo@users.noreply.github.com>
2021-12-14 21:45:25 +0200

    Первая Альфа

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# Python 3.x code write here
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+class App():
+	def __init__(self):
+		Form, Window = uic.loadUiType("main.ui")
+		self.app = QApplication([])
+		self.window = Window()
+		self.form = Form()
+		self.form.setupUi(self.window)
+		
+		#self.form.wbutton.clicked.connect(self.add_listItem)
+		self.init()
+
+	#def add_listItem(self): 
+		#self.form.mtree.addItem(self.form.written.text())
+
+	def init(self):
+		self.window.show()
+		self.app.exec_()
+
+if __name__ == "__main__":
+	newApp = App()
